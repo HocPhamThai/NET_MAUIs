20ddad5f40e2f62397cab2f5e2359daf8fd39246
hocpham <phamthaihoc008@gmail.com>
2025-05-22 01:33:56 +0700

    Project01 - ColorPicker

--- ColorMaker/ColorMaker/MainPage.xaml.cs
+++ ColorMaker/ColorMaker/MainPage.xaml.cs
@@ -1,10 +1,58 @@
-﻿namespace ColorMaker
+﻿
+namespace ColorMaker
 {
     public partial class MainPage : ContentPage
     {
+        bool isGenerateRandom;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (!isGenerateRandom) { 
+                var red = RedSlider.Value;
+                var green = GreenSlider.Value;
+                var blue = BlueSlider.Value;
+
+                var color = Color.FromRgb(red, green, blue);
+
+                SetColor(color);
+            }
+        }
+
+        private void SetColor(Color color)
+        {
+            Container.BackgroundColor = color;
+            btnRandom.BackgroundColor = color;
+            lblHex.Text = color.ToHex();
+        }
+
+        private void btnRandom_Clicked(object sender, EventArgs e)
+        {
+            isGenerateRandom = true;
+            // Random number
+            Random random = new Random();
+            byte red = (byte)random.Next(0, 256);
+            byte green = (byte)random.Next(0, 256);
+            byte blue = (byte)random.Next(0, 256);
+
+            Color color = Color.FromRgb(red, green, blue);
+
+            RedSlider.Value = color.Red;
+            GreenSlider.Value = color.Green;
+            BlueSlider.Value = color.Blue;
+
+            SetColor(color);
+            isGenerateRandom = false;
+        }
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            await Clipboard.Default.SetTextAsync(lblHex.Text);
+            await DisplayAlert("Copied", "Hex color code copied to clipboard.", "OK");
         }
     }
 }
--- ColorMaker/ColorMaker/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/ColorMaker.AssemblyInfo.cs
+++ ColorMaker/ColorMaker/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/ColorMaker.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ColorMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a2abdd402485b43e6882510bde499420199eb2bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ColorMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ColorMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
