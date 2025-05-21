a2abdd402485b43e6882510bde499420199eb2bb
hocpham <phamthaihoc008@gmail.com>
2025-05-22 00:46:43 +0700

    first commit

---
+++ ColorMaker/ColorMaker/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ColorMaker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+        }
+    }
+}
